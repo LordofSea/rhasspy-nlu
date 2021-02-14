57c0bfa6bc504e3abb200d92782c3ebcb1c5587e
LordofSea <44285746+LordofSea@users.noreply.github.com>
2021-02-15 00:43:24 +0100

    remove files

--- bin/intents-to-dot.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-import networkx
-import rhasspynlu
-
-print("Reading graph from stdin...", file=sys.stderr)
-graph = rhasspynlu.gzip_pickle_to_graph(sys.stdin.buffer)
-networkx.drawing.nx_pydot.write_dot(graph, sys.stdout)
--- build/lib/rhasspynlu/jsgf_graph.py
+++ build/lib/rhasspynlu/jsgf_graph.py
@@ -465,6 +465,264 @@ def gzip_pickle_to_graph(in_file: typing.BinaryIO) -> nx.DiGraph:
 # -----------------------------------------------------------------------------
 
 
+@dataclass
+class GraphFsts:
+    """Result from graph_to_fsts."""
+
+    intent_fsts: typing.Dict[str, str]
+    symbols: typing.Dict[str, int]
+    input_symbols: typing.Dict[str, int]
+    output_symbols: typing.Dict[str, int]
+
+
+def graph_to_fsts(
+    graph: nx.DiGraph,
+    eps="<eps>",
+    weight_key="weight",
+    default_weight=0,
+    intent_filter: typing.Optional[typing.Callable[[str], bool]] = None,
+) -> GraphFsts:
+    """Convert graph to OpenFST text format, one per intent."""
+    intent_fsts: typing.Dict[str, str] = {}
+    symbols: typing.Dict[str, int] = {eps: 0}
+    input_symbols: typing.Dict[str, int] = {}
+    output_symbols: typing.Dict[str, int] = {}
+    n_data = graph.nodes(data=True)
+
+    # start state
+    start_node: int = next(n for n, data in n_data if data.get("start"))
+
+    for _, intent_node, edge_data in graph.edges(start_node, data=True):
+        intent_name: str = edge_data["olabel"][9:]
+
+        # Filter intents by name
+        if intent_filter and not intent_filter(intent_name):
+            continue
+
+        final_states: typing.Set[int] = set()
+        state_map: typing.Dict[int, int] = {}
+
+        with io.StringIO() as intent_file:
+            # Transitions
+            for edge in nx.edge_bfs(graph, intent_node):
+                edge_data = graph.edges[edge]
+                from_node, to_node = edge
+
+                # Map states starting from 0
+                from_state = state_map.get(from_node, len(state_map))
+                state_map[from_node] = from_state
+
+                to_state = state_map.get(to_node, len(state_map))
+                state_map[to_node] = to_state
+
+                # Get input/output labels.
+                # Empty string indicates epsilon transition (eps)
+                ilabel = edge_data.get("ilabel", "") or eps
+                olabel = edge_data.get("olabel", "") or eps
+
+                # Map labels (symbols) to integers
+                isymbol = symbols.get(ilabel, len(symbols))
+                symbols[ilabel] = isymbol
+                input_symbols[ilabel] = isymbol
+
+                osymbol = symbols.get(olabel, len(symbols))
+                symbols[olabel] = osymbol
+                output_symbols[olabel] = osymbol
+
+                if weight_key:
+                    weight = edge_data.get(weight_key, default_weight)
+                    print(
+                        f"{from_state} {to_state} {ilabel} {olabel} {weight}",
+                        file=intent_file,
+                    )
+                else:
+                    # No weight
+                    print(
+                        f"{from_state} {to_state} {ilabel} {olabel}", file=intent_file
+                    )
+
+                # Check if final state
+                if n_data[from_node].get("final", False):
+                    final_states.add(from_state)
+
+                if n_data[to_node].get("final", False):
+                    final_states.add(to_state)
+
+            # Record final states
+            for final_state in final_states:
+                print(final_state, file=intent_file)
+
+            intent_fsts[intent_name] = intent_file.getvalue()
+
+    return GraphFsts(
+        intent_fsts=intent_fsts,
+        symbols=symbols,
+        input_symbols=input_symbols,
+        output_symbols=output_symbols,
+    )
+
+
+# -----------------------------------------------------------------------------
+
+
+@dataclass
+class GraphFst:
+    """Result from graph_to_fst."""
+
+    intent_fst: str
+    symbols: typing.Dict[str, int]
+    input_symbols: typing.Dict[str, int]
+    output_symbols: typing.Dict[str, int]
+
+    def write_fst(
+        self,
+        fst_text_path: typing.Union[str, Path],
+        isymbols_path: typing.Union[str, Path],
+        osymbols_path: typing.Union[str, Path],
+    ):
+        """Write FST text and symbol files."""
+        # Write FST
+        Path(fst_text_path).write_text(self.intent_fst)
+
+        # Write input symbols
+        with open(isymbols_path, "w") as isymbols_file:
+            # pylint: disable=E1101
+            for symbol, num in self.input_symbols.items():
+                print(symbol, num, file=isymbols_file)
+
+        # Write output symbols
+        with open(osymbols_path, "w") as osymbols_file:
+            # pylint: disable=E1101
+            for symbol, num in self.output_symbols.items():
+                print(symbol, num, file=osymbols_file)
+
+
+def graph_to_fst(
+    graph: nx.DiGraph,
+    eps="<eps>",
+    weight_key="weight",
+    default_weight=0,
+    intent_filter: typing.Optional[typing.Callable[[str], bool]] = None,
+) -> GraphFst:
+    """Convert graph to OpenFST text format."""
+    symbols: typing.Dict[str, int] = {eps: 0}
+    input_symbols: typing.Dict[str, int] = {}
+    output_symbols: typing.Dict[str, int] = {}
+    n_data = graph.nodes(data=True)
+
+    # start state
+    start_node: int = next(n for n, data in n_data if data.get("start"))
+
+    # Generate FST text
+    with io.StringIO() as fst_file:
+        final_states: typing.Set[int] = set()
+        state_map: typing.Dict[int, int] = {}
+
+        # Transitions
+        for _, intent_node, intent_edge_data in graph.edges(start_node, data=True):
+            intent_olabel: str = intent_edge_data["olabel"]
+            intent_name: str = intent_olabel[9:]
+
+            # Filter intents by name
+            if intent_filter and not intent_filter(intent_name):
+                continue
+
+            assert (
+                " " not in intent_olabel
+            ), f"Output symbol cannot contain whitespace: {intent_olabel}"
+
+            # Map states starting from 0
+            from_state = state_map.get(start_node, len(state_map))
+            state_map[start_node] = from_state
+
+            to_state = state_map.get(intent_node, len(state_map))
+            state_map[intent_node] = to_state
+
+            # Map labels (symbols) to integers
+            isymbol = symbols.get(eps, len(symbols))
+            symbols[eps] = isymbol
+            input_symbols[eps] = isymbol
+
+            osymbol = symbols.get(intent_olabel, len(symbols))
+            symbols[intent_olabel] = osymbol
+            output_symbols[intent_olabel] = osymbol
+
+            if weight_key:
+                weight = intent_edge_data.get(weight_key, default_weight)
+                print(
+                    f"{from_state} {to_state} {eps} {intent_olabel} {weight}",
+                    file=fst_file,
+                )
+            else:
+                # No weight
+                print(f"{from_state} {to_state} {eps} {intent_olabel}", file=fst_file)
+
+            # Add intent sub-graphs
+            for edge in nx.edge_bfs(graph, intent_node):
+                edge_data = graph.edges[edge]
+                from_node, to_node = edge
+
+                # Get input/output labels.
+                # Empty string indicates epsilon transition (eps)
+                ilabel = edge_data.get("ilabel", "") or eps
+                olabel = edge_data.get("olabel", "") or eps
+
+                # Check for whitespace
+                assert (
+                    " " not in ilabel
+                ), f"Input symbol cannot contain whitespace: {ilabel}"
+
+                assert (
+                    " " not in olabel
+                ), f"Output symbol cannot contain whitespace: {olabel}"
+
+                # Map states starting from 0
+                from_state = state_map.get(from_node, len(state_map))
+                state_map[from_node] = from_state
+
+                to_state = state_map.get(to_node, len(state_map))
+                state_map[to_node] = to_state
+
+                # Map labels (symbols) to integers
+                isymbol = symbols.get(ilabel, len(symbols))
+                symbols[ilabel] = isymbol
+                input_symbols[ilabel] = isymbol
+
+                osymbol = symbols.get(olabel, len(symbols))
+                symbols[olabel] = osymbol
+                output_symbols[olabel] = osymbol
+
+                if weight_key:
+                    weight = edge_data.get(weight_key, default_weight)
+                    print(
+                        f"{from_state} {to_state} {ilabel} {olabel} {weight}",
+                        file=fst_file,
+                    )
+                else:
+                    # No weight
+                    print(f"{from_state} {to_state} {ilabel} {olabel}", file=fst_file)
+
+                # Check if final state
+                if n_data[from_node].get("final", False):
+                    final_states.add(from_state)
+
+                if n_data[to_node].get("final", False):
+                    final_states.add(to_state)
+
+        # Record final states
+        for final_state in final_states:
+            print(final_state, file=fst_file)
+
+        return GraphFst(
+            intent_fst=fst_file.getvalue(),
+            symbols=symbols,
+            input_symbols=input_symbols,
+            output_symbols=output_symbols,
+        )
+
+
+# -----------------------------------------------------------------------------
+
 
 def lcm(*nums: int) -> int:
     """Returns the least common multiple of the given integers"""
